273eeb1e4ff0a33dcb1dc93fe0186bc206651f6f
Jasmine Quach <xjasminequach@gmail.com>
2018-01-21 17:05:08 -0600

    InventoryType radio buttons work now

--- app/app.js
+++ app/app.js
@@ -4,8 +4,8 @@ const fabricmuch = angular.module('FabricMuchApp', [
   'ui.router',
   'ui.router.state.events',
   'ui.materialize',
-  'ngFileUpload',
-  'quagga'
+  'ngFileUpload'
+  // 'quagga'
 ])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
